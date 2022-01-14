ea0b7c4367ea0f489d779c2d1d80ceaf9a83220c
tmddusgood <tmddusgood@gmail.com>
2022-01-14 14:35:41 +0900

    fix: 변경 MemberController URI [#4]
    * REST API 규칙에 따른 URI 경로 재설정
    * Swagger API에서 도메인 별 컨트롤러 구분
    
    Related to: #4

--- src/main/java/com/misobuild/controller/MemberController.java
+++ src/main/java/com/misobuild/controller/MemberController.java
@@ -6,9 +6,11 @@ import com.misobuild.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = {"로그인"})
+@Api(tags = {"멤버"})
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
@@ -16,13 +18,13 @@ public class MemberController {
     private final MemberService memberService;
 
     @ApiOperation(value = "회원가입")
-    @PostMapping("/member/signup")
+    @PostMapping("api/member/signup")
     public void registerUser(@RequestBody SignUpRequestDto requestDto) {
         memberService.registerMember(requestDto);
     }
 
     @ApiOperation(value= "로그인")
-    @PostMapping("/member/login")
+    @PostMapping("api/member/login")
     public String login(@RequestBody SignUpRequestDto requestDto) {
         return memberService.login(requestDto);
     }
