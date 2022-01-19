df4c5d067a958e5b282ea712f77ca8cd910acc16
tmddusgood <tmddusgood@gmail.com>
2022-01-19 20:38:16 +0900

    feat: 추가 Utils:Validator KakaoValidator

---
+++ src/main/java/com/misobuild/utils/validator/KakaoValidator.java
@@ -0,0 +1,18 @@
+package com.misobuild.utils.validator;
+
+import com.misobuild.utils.caller.KakaoAuthCaller;
+import lombok.AllArgsConstructor;
+import org.json.JSONObject;
+
+@AllArgsConstructor
+public class KakaoValidator implements Validator{
+    String socialId;
+    String socialToken;
+
+    public Boolean valid() {
+        KakaoAuthCaller kakaoAuthCaller = new KakaoAuthCaller(socialToken);
+        JSONObject jsonObject = kakaoAuthCaller.call();
+        if(jsonObject.get("id").toString().equals(socialId)) return Boolean.TRUE;
+        return Boolean.FALSE;
+    }
+}
