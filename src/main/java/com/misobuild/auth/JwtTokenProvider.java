c1c60c37bcba74d247fd3a9e7edbac666ae372ff
tmddusgood <tmddusgood@gmail.com>
2022-01-20 20:13:02 +0900

    comment: 삭제 auth:KakaoOAuth

--- src/main/java/com/misobuild/auth/JwtTokenProvider.java
+++ src/main/java/com/misobuild/auth/JwtTokenProvider.java
@@ -23,51 +23,43 @@ public class JwtTokenProvider {
 
     @Value("${app.auth.tokenSecret}")
     private String secretKey;
-    private static final long TOKEN_VALID_TIME = 360 * 60 * 1000L;
+    private static final long TOKEN_VALID_TIME = 36000 * 60 * 1000L;
     private final UserDetailsService userDetailsService;
 
-    // 객체 초기화, secretKey를 Base64로 인코딩한다.
     @PostConstruct
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
-    // JWT 토큰 생성
     public String createToken(String memberId, String socialId, String socialType) {
-        Claims claims = Jwts.claims().setSubject(memberId); // JWT payload 에 저장되는 정보단위
-        claims.put("socialId", socialId); // 정보는 key / value 쌍으로 저장된다.
+        Claims claims = Jwts.claims().setSubject(memberId);
+        claims.put("socialId", socialId);
         claims.put("socialType", socialType);
         Date now = new Date();
         return Jwts.builder()
-                .setClaims(claims) // 정보 저장
-                .setIssuedAt(now) // 토큰 발행 시간 정보
-                .setExpiration(new Date(now.getTime() + TOKEN_VALID_TIME)) // set Expire Time
-                .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과
-                // signature 에 들어갈 secret값 세팅
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + TOKEN_VALID_TIME))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
-    // JWT 토큰에서 인증 정보 조회
-    public Authentication getAuthentication(String token) {
-        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+    public Authentication getAuthentication(String serverToken) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(serverToken));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    // 토큰에서 회원 정보 추출
-    public String getUserPk(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    public String getUserPk(String serverToken) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(serverToken).getBody().getSubject();
     }
 
-    // Request의 Header에서 token 값을 가져옵니다. "TOKEN" : "TOKEN 값"
-    //////// FrontEnd와 약속해서 일치시켜야 하는 부분 /////////'
     public String resolveToken(HttpServletRequest request) {
-        return request.getHeader("TOKEN");
+        return request.getHeader("serverToken");
     }
 
-    // 토큰의 유효성 + 만료일자 확인  // -> 토큰이 expire되지 않았는지 True/False로 반환해줌.
-    public boolean validateToken(String jwtToken) {
+    public boolean validateToken(String serverToken) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(serverToken);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (Exception e) {
             return false;
