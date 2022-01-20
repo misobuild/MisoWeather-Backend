cc0dca8a6d3f528312503de5d95e6e2131b66da6
tmddusgood <tmddusgood@gmail.com>
2022-01-20 17:51:16 +0900

    style: 수정 auth:UserDetails code format

--- src/main/java/com/misobuild/auth/UserDetailsImpl.java
+++ src/main/java/com/misobuild/auth/UserDetailsImpl.java
@@ -1,5 +1,6 @@
 package com.misobuild.auth;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.misobuild.domain.member.Member;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,46 +8,56 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class UserDetailsImpl implements UserDetails {
+    @JsonIgnore
     private final Member member;
 
+    @JsonIgnore
     public UserDetailsImpl(Member member) {
         this.member = member;
     }
 
+    @JsonIgnore
     public Member getMember() {
         return member;
     }
 
+    @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
 
+    @JsonIgnore
     @Override
     public String getPassword() {
         return null;
     }
 
+    @JsonIgnore
     @Override
     public String getUsername() {
         return null;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return true;
