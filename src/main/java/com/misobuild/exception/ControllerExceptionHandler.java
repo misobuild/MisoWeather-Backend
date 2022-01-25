6cf475c7ccf78ef1adebaef0e14935a0eb6040e7
tmddusgood <tmddusgood@gmail.com>
2022-01-25 18:51:21 +0900

    feat: 추가 Exception:ControllerExceptionHandler

--- src/main/java/com/misobuild/exception/ControllerExceptionHandler.java
+++ src/main/java/com/misobuild/exception/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
-package com.misobuild.api;
+package com.misobuild.exception;
 
+import com.misobuild.constants.HttpStatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,20 +9,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ResponseBody
     @org.springframework.web.bind.annotation.ExceptionHandler(ApiCustomException.class)
+    // TODO 이거 이외에도 에러코드가 아닌 200 주는 상황이 분명 있을 것.
     public ResponseEntity<Error> exception(ApiCustomException exception){
-        return new ResponseEntity<>(Error.create(exception.getHttpStatusEnum()), HttpStatus.OK);
+        return new ResponseEntity<>(Error.create(exception.getHttpStatusEnum()), HttpStatus.valueOf(exception.getHttpStatusEnum().getStatusCode()));
     }
 
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
     static class Error{
-        private int httpStatus;
+        private int status;
         private String message;
 
         static Error create(HttpStatusEnum exception){
